c4c6a8a99d7952a717c4425853346243500e64b8
Taylor Cox <taylorashtoncox@gmail.com>
2019-11-27 10:06:47 -0500

    Social link sidebar styling

--- src/App.js
+++ src/App.js
@@ -16,33 +16,35 @@ function App() {
         <img src={hello} id="hello-graphic" alt="" />
         <h1 className="sr-only">Hello.</h1>
         <p>You caught me while I'm reworking my portfolio! In the meantime, reach me via one of the channels below:</p>
-        <ul className="flex-list">
-          <li>
-            <a href="https://dribbble.com/taydunworth" target="_blank" aria-label="Visit Tay's Dribbble profile">
-              <FontAwesomeIcon icon={faDribbble} aria-hidden="true" />
-            </a>
-          </li>
-          <li>
-            <a href="https://instagram.com/taylorintheteacups" target="_blank" aria-label="Visit Tay's Instagram">
-              <FontAwesomeIcon icon={faInstagram} aria-hidden="true" />
-            </a>
-          </li>
-          <li>
-            <a href="https://linkedin.com/in/taydunworth" target="_blank" aria-label="Visit Tay's LinkedIn profile">
-              <FontAwesomeIcon icon={faLinkedinIn} aria-hidden="true" />
-            </a>
-          </li>
-          <li>
-            <a href="https://github.com/taydunworth" target="_blank" aria-label="Visit Tay's Github profile">
-              <FontAwesomeIcon icon={faGithub} aria-hidden="true" />
-            </a>
-          </li>
-          <li>
-            <a href="mailto:taydunworth@gmail.com" aria-label="Send Tay an email">
-              <FontAwesomeIcon icon={faEnvelope} aria-hidden="true" />
-            </a>
-          </li>
-        </ul>
+        <div class="social-list-container">
+          <ul className="social-list">
+            <li>
+              <a href="https://dribbble.com/taydunworth" target="_blank" aria-label="Visit Tay's Dribbble profile">
+                <FontAwesomeIcon icon={faDribbble} aria-hidden="true" />
+              </a>
+            </li>
+            <li>
+              <a href="https://instagram.com/taylorintheteacups" target="_blank" aria-label="Visit Tay's Instagram">
+                <FontAwesomeIcon icon={faInstagram} aria-hidden="true" />
+              </a>
+            </li>
+            <li>
+              <a href="https://linkedin.com/in/taydunworth" target="_blank" aria-label="Visit Tay's LinkedIn profile">
+                <FontAwesomeIcon icon={faLinkedinIn} aria-hidden="true" />
+              </a>
+            </li>
+            <li>
+              <a href="https://github.com/taydunworth" target="_blank" aria-label="Visit Tay's Github profile">
+                <FontAwesomeIcon icon={faGithub} aria-hidden="true" />
+              </a>
+            </li>
+            <li>
+              <a href="mailto:taydunworth@gmail.com" aria-label="Send Tay an email">
+                <FontAwesomeIcon icon={faEnvelope} aria-hidden="true" />
+              </a>
+            </li>
+          </ul>
+        </div>
       </main>
     </div>
   );
